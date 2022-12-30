699fba2cb817dba6bcf93de470bb4d3489e9acb4
Jack Rhodes <jack-8729@hotmail.co.uk>
2022-12-30 15:41:59 +0000

    Add initial assets

---
+++ SharedMedia.lua
@@ -0,0 +1,21 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "noot noot!", [[Interface\Addons\RevsShared\Assets\Audio\Noot.mp3]])
+-- -----
+--   STATUSBAR
+-- -----
