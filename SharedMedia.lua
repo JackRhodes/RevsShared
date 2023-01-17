f5da57d5b164b7e5057e550b8f5919ae975d2eb2
Jack Rhodes <hello@jackrhodes.dev>
2023-01-17 00:10:36 +0000

    Add Harci sounds (#8)
    Co-authored-by: Jack Rhodes <jack-8729@hotmail.co.uk>

--- SharedMedia.lua
+++ SharedMedia.lua
@@ -1,21 +1,14 @@
 local LSM = LibStub("LibSharedMedia-3.0") 
 
--- ----- 
--- BACKGROUND 
--- ----- 
-
--- ----- 
---  BORDER 
--- ---- 
-
--- -----
---   FONT
--- -----
-
--- -----
---   SOUND
--- -----
 LSM:Register("sound", "noot noot!", [[Interface\Addons\RevsShared\Assets\Audio\Noot.ogg]])
--- -----
---   STATUSBAR
--- -----
+
+-- Harci Dragons
+LSM:Register("sound", "Harci Black Dragon", [[Interface\Addons\RevsShared\Assets\Audio\HarciBlackDragon.ogg]])
+LSM:Register("sound", "Harci Blue Dragon", [[Interface\Addons\RevsShared\Assets\Audio\HarciBlueDragon.ogg]])
+LSM:Register("sound", "Harci Bronze Dragon", [[Interface\Addons\RevsShared\Assets\Audio\HarciBronzeDragon.ogg]])
+LSM:Register("sound", "Harci Green Dragon", [[Interface\Addons\RevsShared\Assets\Audio\HarciGreenDragon.ogg]])
+LSM:Register("sound", "Harci Red Dragon", [[Interface\Addons\RevsShared\Assets\Audio\HarciRedDragon.ogg]])
+
+-- Harci Other
+LSM:Register("sound", "Harci Coming In Hot", [[Interface\Addons\RevsShared\Assets\Audio\HarciComingInHot.mp3]])
+LSM:Register("sound", "Harci Roar", [[Interface\Addons\RevsShared\Assets\Audio\HarciRoar.mp3]])
